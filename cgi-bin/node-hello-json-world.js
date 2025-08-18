cad2babba7a3abf321f408aeb4b08357a857e548
clydexmarty09 <cmarty@ucsd.edu>
2025-08-18 09:42:09 -0700

    node json edits

--- cgi-bin/node-hello-json-world.js
+++ cgi-bin/node-hello-json-world.js
@@ -1,8 +1,11 @@
-#!/usr/bin/nodejs
-const now = new Date().toISOString; 
-const ip = process.env.REMOTE_ADDR || "unknown"; 
+#!/usr/bin/node
+
 console.log("Cache-Control: no-cache"); 
 console.log("Content-Type: text/json"); 
+console.log(""); 
+
+const now = new Date().toISOString(); 
+const ip = process.env.REMOTE_ADDR || "unknown"; 
 
 const payload ={
     message : "Hello World", ip : ip,  time:now
