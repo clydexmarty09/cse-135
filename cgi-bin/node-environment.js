177736ff424ed068355529a4c899aeeadcf70ff4
clydexmarty09 <cmarty@ucsd.edu>
2025-08-18 09:55:11 -0700

    env node edits

--- cgi-bin/node-environment.js
+++ cgi-bin/node-environment.js
@@ -6,7 +6,7 @@ console.log("");
 console.log("<html><head><title>Environment Variables</title></head>");
 console.log("<body><h1 align=center>Environment Variables</h1><hr/>");
 
-for (const [key, valuye] of Object.entries(process.env)) {
+for (const [key, value] of Object.entries(process.env)) {
   console.log(`${key}=${value}</br>`); 
 }
 
