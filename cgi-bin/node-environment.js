96a207381e6a1b2d1f4387bb7622c0238d66aadc
clydexmarty09 <cmarty@ucsd.edu>
2025-08-18 09:50:06 -0700

    env node edits

--- cgi-bin/node-environment.js
+++ cgi-bin/node-environment.js
@@ -1,6 +1,7 @@
 #!/usr/bin/nodejs
 console.log("Cache-Control: no-cache"); 
 console.log("Content-Type: text/html");
+console.log(""); 
 
 console.log("<html><head><title>Environment Variables</title></head>");
 console.log("<body><h1 align=center>Environment Variables</h1><hr/>");
