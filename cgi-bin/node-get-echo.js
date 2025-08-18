e2288dbd45f18320c7ba6f93d5818c0d4a95d5c4
clydexmarty09 <cmarty@ucsd.edu>
2025-08-18 11:27:01 -0700

    node get echo edits

--- cgi-bin/node-get-echo.js
+++ cgi-bin/node-get-echo.js
@@ -5,10 +5,11 @@ console.log("");
 
 
 console.log("<html><body>"); 
-console.log("<h1 align=center>GET Request Echo></h1>\n")
+console.log("<h1 align=center>GET Request Echo</h1>\n")
 console.log("<hr />\n")
 
+const queryString = process.env.QUERY_STRING || "";  // read the query string 
 
-
+console.log("<p> Raw Query String: " + qs + " </p>"); 
 
 console.log("</body></html>"); 
