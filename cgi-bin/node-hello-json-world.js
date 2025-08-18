1d7ba366bb6264bf750a57f5858393e3cc68d3a5
clydexmarty09 <cmarty@ucsd.edu>
2025-08-17 23:02:14 -0700

    added hello-json-world

---
+++ cgi-bin/node-hello-json-world.js
@@ -0,0 +1,34 @@
+const { createServer } = require('node:http'); 
+const hostname = '127.0.0.1'; 
+const port = 3000; 
+ 
+
+const server = createServer((req, res)=> {
+
+
+    //const for date
+    const now = new Date();
+    console.log(req.headers);   
+    res.statusCode = 200; 
+    res.setHeader('Content-Type','application/json'); 
+   
+    //grab user's id 
+    const ip= req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const payload = {
+
+        message: "Hellow World", 
+        ip: ip,
+        printedAt: now.toISOString()
+    }; 
+
+    res.end(JSON.stringify(payload)); 
+    
+
+}); 
+
+server.listen(port, hostname, () => {
+
+    console.log(`Server running at http://${hostname}:${port}/`);
+   
+
+});
