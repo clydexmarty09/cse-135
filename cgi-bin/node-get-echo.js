45a7eb6e2e24a575cf0eecb73e5279b601e657c1
clydexmarty09 <cmarty@ucsd.edu>
2025-08-18 10:07:10 -0700

    node get echo edits

---
+++ cgi-bin/node-get-echo.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+console.log("Cache-Control: no-cache"); 
+console.log("Content-Type: text/html");
+console.log(""); 
+
+
+console.log("<html><body>"); 
+console.log("<h1 align=center>GET Request Echo></h1>\n")
+console.log("<hr />\n")
+
+
+
+
+console.log("</body></html>"); 
