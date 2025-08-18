d93fbe4c71c7c038777f97406e11c5d54c4e0f12
clydexmarty09 <cmarty@ucsd.edu>
2025-08-18 09:00:45 -0700

    eos outpit header debugging?

--- cgi-bin/node-hello-html-world.js
+++ cgi-bin/node-hello-html-world.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const now = new Date().toISOString; 
-const ip = process.env.REMOTE_ADDR || "unknown"; 
 console.log("Cache-Control: no-cache"); 
 console.log("Content-Type: text/html"); 
+
+const ip = process.env.REMOTE_ADDR || "unknown"; 
 
 console.log("<html><body>");
 console.log("<h1>Hello World from Node CGI!</h1>");
