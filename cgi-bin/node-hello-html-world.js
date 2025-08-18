daa16036c895a4ff826388c3137c82a551c2a127
clydexmarty09 <cmarty@ucsd.edu>
2025-08-18 13:05:45 -0700

    messed up

--- cgi-bin/node-hello-html-world.js
+++ cgi-bin/node-hello-html-world.js
@@ -1,5 +1,5 @@
-#!/usr/bin/node
 
+/*
 const now = new Date().toISOString(); 
 
 //REQUIRED HEADERS 
@@ -14,3 +14,20 @@ console.log("<h1>Hello World from Node CGI!</h1>");
 console.log(`<p>Your IP is: ${ip}</p>`);
 console.log(`<p>Current time: ${now}</p>`);
 console.log("</body></html>");
+*/ 
+
+const http = require('node:http');
+
+http.createServer((req, res) => {
+  const now = new Date().toISOString();
+  const ip = req.socket.remoteAddress;
+
+  res.writeHead(200, {'Content-Type': 'text/html', 'Cache-Control': 'no-cache'});
+  res.end(`
+    <html><body>
+      <h1>Hello World from Node Server!</h1>
+      <p>Your IP is: ${ip}</p>
+      <p>Current time: ${now}</p>
+    </body></html>
+  `);
+}).listen(3000, '127.0.0.1');
