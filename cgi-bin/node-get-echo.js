ad05dfd4fdbe60dc376dc5945816947759ec1aac
clydexmarty09 <cmarty@ucsd.edu>
2025-08-18 17:16:32 -0700

    editing get echo

--- cgi-bin/node-get-echo.js
+++ cgi-bin/node-get-echo.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+/* 
 console.log("Cache-Control: no-cache"); 
 console.log("Content-Type: text/html");
 console.log(""); 
@@ -12,4 +13,22 @@ const qs = process.env.QUERY_STRING || "";  // read the query string
 
 console.log("<p> Raw Query String: " + qs + " </p>"); 
 
-console.log("</body></html>"); 
+console.log("</body></html>"); */ 
+
+const http =  require('node:http');  
+http.createServer((req, res) => {
+res.writeHead(200, {'Content-Type': 'text/html', 'Cache-Control': 'no-cache'});
+const qs = process.env.QUERY_STRING || ""; 
+ 
+res.write(`
+ 
+  <html><body><h1 align=center> GET Echo </h1><hr><pre> 
+ 
+ `);
+
+ res.write('<p> Raw Query String:' + qs + ' </p>'); 
+
+ res.write(`</pre></body></html>`); 
+ res.end(); 
+    
+}).listen(3003,  '127.0.0.1');
